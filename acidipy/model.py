e6c637481a585c9ed101fb6fc44cd899a15699b5
Hye-Churn, Jang <hyjang@cisco.com>
2016-11-03 13:49:19 +0900

    thread on py3

--- acidipy/model.py
+++ acidipy/model.py
@@ -6,14 +6,11 @@ Created on 2016. 10. 18.
 
 import re
 import ssl
-import time
 import json
-import threading
 
 from websocket import create_connection
-try: from Queue import Queue
-except: from queue import Queue
 
+from .common import SystemThread, SchedTask, Scheduler
 from .session import Session
 from .static import *
 
@@ -618,80 +615,8 @@ class PhysIfObject(AcidipyObject, AcidipyObjectParent): pass
 #############################################################################################################
 
 #===============================================================================
-# System Thread
-#===============================================================================
-class SystemThread(threading.Thread):
-    
-    def __init__(self):
-        threading.Thread.__init__(self)
-        self._tb_sw = False
-        
-    def start(self):
-        if not self._tb_sw:
-            self._tb_sw = True
-            threading.Thread.start(self)
-    
-    def stop(self):
-        if self._tb_sw:
-            self._tb_sw = False
-            self._Thread__stop()
-            self.join()
-        
-    def run(self):
-        while self._tb_sw: self.thread()
-            
-    def thread(self): pass
-
-#===============================================================================
-# Scheduler
-#===============================================================================
-class SchedTask:
-    def __init__(self, tick):
-        self.schedtask_tick = tick
-        self.schedtask_cur = 0
-        
-    def __sched_wrapper__(self, sched_tick):
-        self.schedtask_cur += sched_tick
-        if self.schedtask_cur >= self.schedtask_tick:
-            self.schedtask_cur = 0
-            self.sched()
-            
-    def sched(self): pass
-
-class Scheduler(SystemThread):
-    
-    def __init__(self, tick):
-        SystemThread.__init__(self)
-        self.tick = tick
-        self.queue = []
-        self.regreq = Queue()
-    
-    def thread(self):
-        start_time = time.time()
-        while not self.regreq.empty():
-            task = self.regreq.get()
-            self.queue.append(task)
-        for task in self.queue:
-            try: task.__sched_wrapper__(self.tick)
-            except: continue
-        end_time = time.time()
-        sleep_time = self.tick - (end_time - start_time)
-        if sleep_time > 0: time.sleep(sleep_time)
-        
-            
-    def register(self, task):
-        self.regreq.put(task)
-        
-    def unregister(self, task):
-        sw_stat = self._tb_sw
-        if sw_stat: self.stop()
-        if task in self.queue: self.queue.remove(task)
-        if sw_stat: self.start()
-
-#===============================================================================
 # Subscriber
 #===============================================================================
-
 class Subscriber(SystemThread, SchedTask):
     
     def __init__(self, controller):
