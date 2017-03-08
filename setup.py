1ee0836cffb06ac955f192292d1738763b1b8c88
Hye-Churn, Jang <hyjang@cisco.com>
2017-03-08 17:57:07 +0900

    change setup description

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ def read(fname): return open(os.path.join(os.path.dirname(__file__), fname)).rea
 
 setup(
     name='acidipy',
-    version='0.10.9',
+    version='0.10.10',
     license='Apache 2.0',
     author='Hyechurn Jang',
     author_email='hyjang@cisco.com',
     url='https://github.com/HyechurnJang/acidipy',
-    description='ACI Developing Interface for PYthon',
+    description='Cisco ACI Developing Interface for PYthon',
     long_description=read('README'),
     packages=['acidipy'],
     install_requires=['gevent', 'pygics', 'pyaml', 'requests', 'websocket-client'],
